3eee18c32db0628d60ef974e72cbad406757b6d3
Gonzalo Martone <gmartone@itba.edu.ar>
2022-09-05 13:19:26 -0300

    DEV: slave.c

--- slave.c
+++ slave.c
@@ -1,22 +1,58 @@
+#define _GNU_SOURCE     // SOURCE: man getline
+#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-char * solve(char * filename);
+#define MAX_CMD_LEN 2048
+#define MD5_OUT_LEN 32
+#define CMD_FMT_STR "md5sum %s"
+#define OUT_FMT_STR "File: %s, PID: %d, md5: %s\n"
 
+// Filename read from stdin
+// Filename, PID & md5sum get writen to stdout
 int main (int argc, char *argv[]) {
-    char cmd_buff[32];
-    char * filename;
-    int * linecap;
+    // TODO: ver si usamos setvbuf (mejor select?)
+    // setvbuf(stdout, NULL, _IONBF, 0);
 
-    getline(&filename, &linecap, stdin > 0);
-    sprintf(cmd_buff, "md5sum %s", filename);
+    // === geline ===
+    char * filename = NULL;
+    size_t len = 0;
+    ssize_t nread;
+    // ===
 
-    //llamar solve y devolver al pipe
+    // === cmd ===
+    char cmd_buff[MAX_CMD_LEN]   = {0}; // command to execute
+    char md5_output[MD5_OUT_LEN] = {0}; // command output
+    // ===
 
+    // read filename from stdin
+    while((nread = getline(&filename, &len, stdin)) > 0) {
 
-    return 0;
-}
+        // TODO: check the line
+        // remove the '\n'
+        filename[nread - 1] = '\0';
+        // printf("%s", filename); TODO: remove handy debug print ;P
 
-char * solve(char * filename){
-    char * md5 = md5sum(filename);
-    return md5;
+        // build the command to execute by
+        // printing the command to cmd_buff
+        if(sprintf(cmd_buff, CMD_FMT_STR, filename) < 0) {
+            fprintf(stderr, "Error in sprintf");
+            exit(EXIT_FAILURE);
+        }
+
+        // TODO: execute the command
+        // TODO: catch the output -> md5_output
+
+        // format output for stdout
+        fprintf(stdout, OUT_FMT_STR, filename, getpid(), md5_output);
+
+        // TODO: cleanup
+    }
+
+    // TODO: check for more necessary 'frees'
+
+    free(filename);         // SOURCE: man geline
+    close(STDIN_FILENO);    // close pipe
+    return EXIT_SUCCESS;
 }
