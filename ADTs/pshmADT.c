9c57e407dd51d3ec25121384b782b7184c526001
Cuinardium <sballerini@itba.edu.ar>
2022-09-07 16:05:28 -0300

    DEV: Implemented view & pshm r/w

--- ADTs/pshmADT.c
+++ ADTs/pshmADT.c
@@ -1,25 +1,30 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+
 #include "pshmADT.h"
+#include <errno.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <semaphore.h>
 #include <stdio.h>
-#include <sys/mman.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
-#include <errno.h>
-#include <limits.h>
-#include <fcntl.h>
+#include <sys/mman.h>
 #include <sys/stat.h>
-#include <semaphore.h>
+#include <unistd.h>
 
 #define MAX_PSM_SIZE INT_MAX
+#define END_IDENTIFIER '\n'
 
 typedef int fd_t;
 
 typedef struct pshmCDT {
     // char name[MAX_NAME_LEN];
-    const char * shm_name;
-    const char * sem_name;
-    sem_t * sem;
-    char * addr;
+    const char *shm_name;
+    const char *sem_name;
+    sem_t *sem;
+    char *addr;
     int write_off;
     int read_off;
     fd_t fd;
@@ -31,23 +36,24 @@ static int decCounter(pshmADT pshm);
 static int incCounter(pshmADT pshm);
 
 // TODO: SEMAPHORES
-pshmADT newPshm(const char *shm_name, const char * sem_name, int oflag, mode_t mode) {
-    if(shm_name == NULL) {
+pshmADT newPshm(const char *shm_name, const char *sem_name, int oflag, mode_t mode) {
+
+    if (shm_name == NULL) {
         errno = EINVAL; // Invalid argument
         return NULL;
     }
     pshmADT new = malloc(sizeof(pshmCDT));
-    if(new == NULL) {
+    if (new == NULL) {
         return NULL;
     }
 
     // TODO: CHECK FAILURE
-    //strncpy(new->shm_name, name, MAX_NAME_LEN - 1);
+    // strncpy(new->shm_name, name, MAX_NAME_LEN - 1);
     new->shm_name = shm_name;
     new->sem_name = sem_name;
 
     new->fd = shm_open(new->shm_name, oflag, mode);
-    if(new->fd == -1) {
+    if (new->fd == -1) {
         free(new);
         return NULL;
     }
@@ -57,36 +63,35 @@ pshmADT newPshm(const char *shm_name, const char * sem_name, int oflag, mode_t m
     new->size = MAX_PSM_SIZE;
     new->counter_pos = new->size;
 
-    if(oflag & O_CREAT) {
+    if (oflag & O_CREAT) {
         // If the user wants to create it, use ftruncate
-        if(ftruncate(new->fd, new->size*sizeof(char)) == -1) {
+        if (ftruncate(new->fd, new->size * sizeof(char)) == -1) {
             freePshm(new);
             return NULL;
         }
         new->sem = sem_open(new->sem_name, O_CREAT, mode, 0);
-        if(new->sem == SEM_FAILED) {
+        if (new->sem == SEM_FAILED) {
             freePshm(new);
             return NULL;
         }
     } else {
         new->sem = sem_open(new->sem_name, 0);
-        if(new->sem == SEM_FAILED) {
+        if (new->sem == SEM_FAILED) {
             freePshm(new);
             return NULL;
         }
     }
 
     int prot = PROT_READ; // default
-    if(oflag & O_RDWR) {
+    if (oflag & O_RDWR) {
         prot = prot | PROT_WRITE;
     }
 
-    new->addr = mmap(NULL, new->size*sizeof(char), prot, MAP_SHARED, new->fd, 0);
-    if(new->addr == (void *) -1) {
+    new->addr = mmap(NULL, new->size * sizeof(char), prot, MAP_SHARED, new->fd, 0);
+    if (new->addr == (void *)-1) {
         freePshm(new);
         return NULL;
     }
-
 
     // leave space for a current users counter
     new->size--;
@@ -95,35 +100,66 @@ pshmADT newPshm(const char *shm_name, const char * sem_name, int oflag, mode_t m
     return new;
 }
 
-// TODO: SEMAPHORES
-size_t writePshm(pshmADT pshm, const char * buff, size_t bytes) {
-    if(pshm == NULL) {
+// Writes a line to the shared memory
+size_t writePshm(pshmADT pshm, const char *buff, size_t bytes) {
+    if (pshm == NULL) {
         errno = EINVAL;
-        return 0;
+        return -1;
     }
-    ssize_t bytes_writen = 0;
+    
+    sem_wait(pshm->sem);
+    // Critical section
+    ssize_t bytes_writen;
+    for (bytes_writen = 0; bytes_writen < bytes; bytes_writen++) {
 
-    // TODO: write & semaphores
+        // Stop writing if string ends
+        if (buff[bytes_writen] == '\0') {
+            break;
+        }
+
+        pshm->addr[pshm->write_off] = buff[bytes_writen];
+        pshm->write_off = (pshm->write_off + 1) % pshm->size; // circular buffer
+
+        // Stop writing at end of line
+        if (buff[bytes_writen] == END_IDENTIFIER) { 
+            break;
+        }
+    }
+
+    sem_post(pshm->sem);
 
     return bytes_writen;
 }
 
-// TODO: SEMAPHORES
-size_t readPshm(pshmADT pshm, char * buff, size_t bytes) {
-    if(pshm == NULL) {
+// Reads a line from the shared memory
+size_t readPshm(pshmADT pshm, char *buff, size_t bytes) {
+    if (pshm == NULL) {
         errno = EINVAL;
-        return 0;
+        return -1;
     }
 
-    ssize_t bytes_read = 0;
+    sem_wait(pshm->sem);
+    // Critical section
+    ssize_t bytes_read;
+    // Read byte for byte until bytes is reached, if byte equals END_IDENTIFIER,
+    // stop reading
+    for (bytes_read = 0; bytes_read < bytes - 1; bytes_read++) {
+        buff[bytes_read] = pshm->addr[pshm->read_off];
+        pshm->read_off = (pshm->read_off + 1) % pshm->size; // circular buffer
 
-    // TODO: read & semaphores
+        if (pshm->addr[pshm->read_off] == END_IDENTIFIER) {
+            break;
+        }
+    }
 
-    return bytes_read;
+    sem_post(pshm->sem);
+
+    buff[bytes_read] = '\0';
+    return bytes_read + 1;
 }
 
 void freePshm(pshmADT pshm) {
-    if(pshm == NULL) {
+    if (pshm == NULL) {
         errno = EINVAL;
         return;
     }
@@ -131,7 +167,7 @@ void freePshm(pshmADT pshm) {
 
     munmap(pshm->addr, pshm->size);
     close(pshm->fd);
-    if(remaining_users == 0) {
+    if (remaining_users == 0) {
         // CHECK: shm_unlink (only last user should close it)
         // maybe solvabe using a counter in the actual memory space
         shm_unlink(pshm->shm_name);
@@ -153,3 +189,4 @@ static int incCounter(pshmADT pshm) {
     int qty_users = pshm->addr[pshm->counter_pos]++;
     return qty_users;
 }
+
--- view.c
+++ view.c
@@ -2,20 +2,49 @@
 // PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
 #include <stdio.h>
 #include <stdlib.h>
+#include <semaphore.h>
+#include "ADTs/pshmADT.h"
+#include "ADTs/smADT.h"
+
+#define BUFFER_SIZE 1024
+
+static void failNExit(const char *msg);
 
 int main (int argc, char *argv[]) {
-    switch(argc) {
-        case 1:
-            // TODO: handle reading shared memory connection info
-            break;
-        case 2:
-            // TODO: handle recieving shared memory connection info via argument
-            break;
-        default:
-            fprintf(stderr, "Wrong view usage\n");
-            exit(EXIT_FAILURE);
+
+    // Validate arguments
+    if (argc != 3) {
+        fprintf(stderr, "Wrong view usage\n");
+        exit(EXIT_FAILURE);
+    } 
+
+    pshmADT pshm = newPshm(argv[1], argv[2], O_RDWR, 0666);
+    if (pshm == NULL) {
+        failNExit("Error creating pshm");
     }
+
+    int fileAmount = strtol(argv[3], NULL, 10);
+
+    char buffer[BUFFER_SIZE] = {0};
+
+    for (int i = 0; i < fileAmount; i++) {
+
+        int bytesRead = readPshm(pshm, buffer, BUFFER_SIZE);
+        if (bytesRead == -1) {
+            failNExit("Error reading pshm");
+        }
+
+        printf("%s\n", buffer);
+    } 
+
+    freePshm(pshm);
     return 0;
+}
+
+
+static void failNExit(const char *msg) {
+    perror(msg);
+    exit(EXIT_FAILURE);
 }
 
 // This is a personal academic project. Dear PVS-Studio, please check it.
