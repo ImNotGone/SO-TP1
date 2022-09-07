0f2dd40249bf5db2b9332eead96daa0ddf8b007c
martinhecht01 <mhecht@itba.edu.ar>
2022-09-07 16:45:24 -0300

    slave modifications

--- slave.c
+++ slave.c
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
+#include <sys/stat.h>
 
 #define MAX_CMD_LEN 2048
 #define MD5_OUT_LEN 32
@@ -15,6 +16,8 @@
 #define OUT_FMT_STR "File: %s, PID: %d, md5: %s\n"
 
 void formatOutput(char * output, FILE * stream);
+static void solve(const char * filename);
+static int checkPath(char * path);
 static void failNExit(const char *msg);
 
 // Filename read from stdin
@@ -27,40 +30,16 @@ int main (int argc, char *argv[]) {
     ssize_t nread;
     // ===
 
-    // === cmd ===
-    char cmd_buff[MAX_CMD_LEN]   = {0};   // command to execute
-    char md5_output[MD5_OUT_LEN+1] = {0}; // command output
-    // ===
 
     setvbuf(stdout, NULL, _IONBF, 0);
 
     // read filename from stdin
     while((nread = getline(&filename, &len, stdin)) > 0) {
-
         // remove the '\n'
         filename[nread - 1] = '\0';
-
-        // build the command to execute by
-        // printing the command to cmd_buff
-        if(sprintf(cmd_buff, CMD_FMT_STR, filename) < 0)
-            failNExit("Error in sprintf");
-
-        // execute the command
-        FILE * commandOutput = popen(cmd_buff,"r");
-        if (commandOutput == NULL)
-            failNExit("popen function error");
-
-        // catch the output -> md5_output
-        formatOutput(md5_output,commandOutput);
-
-        // format output for stdout
-        fprintf(stdout, OUT_FMT_STR, filename, getpid(), md5_output);
-
-        if (pclose(commandOutput) == -1)
-            failNExit("pclose function error");
-
-        // TODO: cleanup
+        solve(filename);
     }
+
 
     // catch getline error if ocurred
     if(errno == EINVAL || errno == ENOMEM) {
@@ -87,6 +66,43 @@ void formatOutput(char * output, FILE * stream){
 static void failNExit(const char *msg) {
     fprintf(stderr, "%s", msg);
     exit(EXIT_FAILURE);
+}
+
+static void solve(const char * filename){
+
+    if(!checkPath(filename)){
+        fprintf(stdout, "%s is not a file\n", filename);
+        return;
+    }
+     // === cmd ===
+    char cmd_buff[MAX_CMD_LEN]   = {0};   // command to execute
+    char md5_output[MD5_OUT_LEN+1] = {0}; // command output
+    // ===
+
+    // build the command to execute by
+    // printing the command to cmd_buff
+    if(sprintf(cmd_buff, CMD_FMT_STR, filename) < 0)
+        failNExit("Error in sprintf");
+
+    // execute the command
+    FILE * commandOutput = popen(cmd_buff,"r");
+    if (commandOutput == NULL)
+        failNExit("popen function error");
+
+    // catch the output -> md5_output
+    formatOutput(md5_output,commandOutput);
+
+    // format output for stdout
+    fprintf(stdout, OUT_FMT_STR, filename, getpid(), md5_output);
+    if (pclose(commandOutput) == -1)
+        failNExit("pclose function error");
+
+    // TODO: cleanup
+}
+
+static int checkPath(char * path){
+    struct stat validation;
+    return stat(path, &validation) >= 0 && S_ISREG(validation.st_mode);
 }
 
 // This is a personal academic project. Dear PVS-Studio, please check it.
